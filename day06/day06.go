0322b3183ea49de854356c20271a73e1b9efaf20
Benjamin <f.benjamin.larsson@gmail.com>
2024-12-06 21:08:05 +0100

    Day 06 part two

--- day06/day06.go
+++ day06/day06.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -24,6 +26,10 @@ func StrToInt(str string) int {
 	}
 
 	return num
+}
+
+func ReplaceAt(str string, index int, new rune) string {
+	return str[:index] + string(new) + str[index+1:]
 }
 
 //go:embed input.txt
@@ -59,6 +65,7 @@ findStartingPos:
 
 	width := len(lines[0])
 	height := len(lines)
+
 	for {
 		visited[[2]int{guardX, guardY}] = true
 
@@ -89,6 +96,85 @@ findStartingPos:
 	fmt.Println(len(visited))
 }
 
-func partTwo() {
+func isLoop(lines []string, guardX int, guardY int, obsX int, obsY int) bool {
+	visitedWithDirections := make(map[[2]int]int)
 
+	guardDirection := 0 // 0 ^, 1 >, 2 v, 3 <
+	width := len(lines[0])
+	height := len(lines)
+	for i := 0; i < 100000000; i++ {
+		if dir, beenHere := visitedWithDirections[[2]int{guardX, guardY}]; beenHere && dir&(1<<guardDirection) != 0 {
+			return true
+		}
+
+		visitedWithDirections[[2]int{guardX, guardY}] |= 1 << guardDirection
+
+		newX, newY := guardX, guardY
+
+		switch guardDirection {
+		case 0:
+			newY--
+		case 1:
+			newX++
+		case 2:
+			newY++
+		case 3:
+			newX--
+		}
+
+		if newX < 0 || newX > width-1 || newY < 0 || newY > height-1 {
+			return false
+		}
+
+		if lines[newY][newX] == '#' || newX == obsX && newY == obsY {
+			guardDirection = (guardDirection + 1) % 4
+		} else {
+			guardX, guardY = newX, newY
+		}
+	}
+
+	fmt.Println("Time paradox detected when placing obstacle at", obsX, obsY)
+	return false
+}
+
+func partTwo() {
+	defer PrintTimeSince(time.Now())
+
+	lines := SplitByLines(input)
+
+	guardX := 0
+	guardY := 0
+
+findStartingPos:
+	for y, line := range lines {
+		for x, c := range line {
+			if c == '^' {
+				guardX = x
+				guardY = y
+				break findStartingPos
+			}
+		}
+	}
+
+	var numPossibilities atomic.Uint64
+	var wg sync.WaitGroup
+
+	for y, line := range lines {
+		for x, c := range line {
+			if c == '.' {
+				wg.Add(1)
+				go func(x, y int) {
+					defer wg.Done()
+
+					if isLoop(lines, guardX, guardY, x, y) {
+						numPossibilities.Add(1)
+					}
+				}(x, y)
+			}
+		}
+	}
+
+	wg.Wait()
+
+	fmt.Println(numPossibilities.Load())
 }
