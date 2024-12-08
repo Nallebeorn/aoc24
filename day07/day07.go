589fd06203481683462432a6c791ecfb64a2695c
Benjamin <f.benjamin.larsson@gmail.com>
2024-12-08 15:54:34 +0100

    Day 07 part one

---
+++ day07/day07.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func PrintTimeSince(start time.Time) {
+	fmt.Println(time.Since(start))
+}
+
+func SplitByLines(data string) []string {
+	normalized := strings.ReplaceAll(data, "\r\n", "\n")
+	return strings.Split(normalized, "\n")
+}
+
+func StrToInt(str string) int64 {
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return num
+}
+
+type Operator byte
+
+const (
+	Start    Operator = '_'
+	Add      Operator = '+'
+	Multiply Operator = '*'
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	fmt.Println("Day 07")
+	partOne()
+	partTwo()
+}
+
+func verifyEquation(testValue int64, operands []int64, operator Operator, accumulator int64) bool {
+	switch operator {
+	case Add:
+		accumulator += operands[0]
+	case Multiply:
+		accumulator *= operands[0]
+	case Start:
+		accumulator = operands[0]
+	default:
+		panic(fmt.Sprintln("Unknown operator", operator))
+	}
+
+	if len(operands) == 1 {
+		return accumulator == testValue
+	}
+
+	if accumulator > testValue {
+		return false
+	}
+
+	if verifyEquation(testValue, operands[1:], Add, accumulator) ||
+		verifyEquation(testValue, operands[1:], Multiply, accumulator) {
+		return true
+	}
+
+	return false
+}
+
+func partOne() {
+	defer PrintTimeSince(time.Now())
+
+	lines := SplitByLines(input)
+
+	var result int64
+
+	for _, line := range lines {
+		split := strings.Split(line, ":")
+		testValue := StrToInt(split[0])
+		operands := make([]int64, 0, 16)
+		for _, v := range strings.Fields(split[1]) {
+			operands = append(operands, StrToInt(v))
+		}
+
+		if verifyEquation(testValue, operands, Start, 0) {
+			result += testValue
+		}
+	}
+
+	fmt.Println(result)
+
+}
+
+func partTwo() {
+	defer PrintTimeSince(time.Now())
+}
