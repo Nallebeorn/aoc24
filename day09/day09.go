8c19cb5c7a58bdf52c948bcdfbed331f85be7391
Benjamin <f.benjamin.larsson@gmail.com>
2024-12-09 23:51:45 +0100

    Day 09 part two

--- day09/day09.go
+++ day09/day09.go
@@ -76,6 +76,61 @@ func partOne() {
 	fmt.Println(checksum)
 }
 
+type block struct {
+	id   int
+	size int
+}
+
 func partTwo() {
 	defer PrintTimeSince(time.Now())
+
+	blocks := make([]block, 0)
+	for i, c := range input {
+		var id int
+		if i%2 == 0 {
+			id = i / 2
+		} else {
+			id = -1
+		}
+
+		size := StrToInt(string(c))
+		blocks = append(blocks, block{id, size})
+	}
+
+	for i := len(blocks) - 1; i >= 0; i-- {
+		blk := blocks[i]
+
+		if blk.id == -1 {
+			continue
+		}
+
+		firstFree := slices.IndexFunc(blocks, func(b block) bool {
+			return b.id == -1 && b.size >= blk.size
+		})
+
+		if firstFree != -1 && firstFree < i {
+			remainingSpace := blocks[firstFree].size - blk.size
+			blocks[i].id = -1
+			blocks[firstFree] = blk
+			if remainingSpace > 0 {
+				blocks = slices.Insert(blocks, firstFree+1, block{-1, remainingSpace})
+			}
+		}
+	}
+
+	// fmt.Println(blocks)
+
+	checksum := 0
+
+	i := 0
+	for _, block := range blocks {
+		for j := 0; j < block.size; j++ {
+			if block.id != -1 {
+				checksum += i * block.id
+			}
+			i++
+		}
+	}
+
+	fmt.Println(checksum)
 }
