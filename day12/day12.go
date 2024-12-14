e250df0306770e33a7583b686dc97913fa9ae745
Benjamin <f.benjamin.larsson@gmail.com>
2024-12-14 22:44:02 +0100

    Day 12 part two

--- day12/day12.go
+++ day12/day12.go
@@ -118,6 +118,114 @@ func partOne() {
 	fmt.Println(sum)
 }
 
+func getPrice2(plot *plot) int {
+	if plot.visited {
+		return 0
+	}
+
+	area, perimeter := getAreaAndSides(plot)
+	// fmt.Println(string(plot.plant), ":", plot.x, plot.y, area, "*", perimeter, "=", area*perimeter)
+	return area * perimeter
+}
+
+func getAreaAndSides(p *plot) (int, int) {
+	if p.visited {
+		return 0, 0
+	}
+
+	p.visited = true
+
+	area := 1
+
+	isEdge := func(other *plot) bool {
+		return other == nil || other.plant != p.plant
+	}
+
+	up := isEdge(p.up)
+	down := isEdge(p.down)
+	right := isEdge(p.right)
+	left := isEdge(p.left)
+
+	corners := 0
+
+	if up && right {
+		corners++
+	}
+	if right && down {
+		corners++
+	}
+	if down && left {
+		corners++
+	}
+	if left && up {
+		corners++
+	}
+
+	if !up && !right && p.up.right.plant != p.plant {
+		corners++
+	}
+	if !right && !down && p.right.down.plant != p.plant {
+		corners++
+	}
+	if !down && !left && p.down.left.plant != p.plant {
+		corners++
+	}
+	if !left && !up && p.left.up.plant != p.plant {
+		corners++
+	}
+
+	// if corners != 0 && p.plant == 'C' {
+	// 	fmt.Println(string(p.plant), "corners!", p.x, p.y, corners)
+	// }
+
+	for _, neighbour := range []*plot{p.left, p.right, p.up, p.down} {
+		if !isEdge(neighbour) && !neighbour.visited {
+			na, nc := getAreaAndSides(neighbour)
+			area += na
+			corners += nc
+		}
+	}
+
+	return area, corners
+}
+
 func partTwo() {
 	defer PrintTimeSince(time.Now())
+
+	garden := make([]plot, 0)
+	lines := SplitByLines(input)
+	width := len(lines[0])
+	height := len(lines)
+	for y, line := range lines {
+		for x, plant := range line {
+			garden = append(garden, plot{plant: plant, x: x, y: y})
+		}
+	}
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			plot := &garden[y*width+x]
+			if x > 0 {
+				plot.left = &garden[y*width+x-1]
+			}
+			if x < width-1 {
+				plot.right = &garden[y*width+x+1]
+			}
+			if y > 0 {
+				plot.up = &garden[y*width+x-width]
+			}
+			if y < height-1 {
+				plot.down = &garden[y*width+x+width]
+			}
+		}
+	}
+
+	sum := 0
+	for i := range garden {
+		sum += getPrice2(&garden[i])
+	}
+
+	// sum = getPrice(&garden[0])
+
+	fmt.Println(sum)
 }
