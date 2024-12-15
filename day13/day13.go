b659762273165829f8a1bd8b21f519d053022875
Benjamin <f.benjamin.larsson@gmail.com>
2024-12-15 19:59:12 +0100

    Day 13 part two

--- day13/day13.go
+++ day13/day13.go
@@ -104,6 +104,55 @@ func partOne() {
 	fmt.Println(sum)
 }
 
+func getCost2(machine machine) (int, bool) {
+	a := (machine.bx*machine.prizey - machine.prizex*machine.by) / (machine.bx*machine.ay - machine.ax*machine.by)
+	b := (machine.prizex*machine.ay - machine.ax*machine.prizey) / (machine.bx*machine.ay - machine.ax*machine.by)
+
+	x := machine.ax*a + machine.bx*b
+	y := machine.ay*a + machine.by*b
+
+	if x == machine.prizex && y == machine.prizey {
+		return a*3 + b, true
+	}
+
+	return 0, false
+}
+
 func partTwo() {
 	defer PrintTimeSince(time.Now())
+
+	machines := make([]machine, 0)
+
+	lines := SplitByLines(input)
+	for i := 0; i < len(lines); i += 4 {
+		var machine machine
+		var fields []string
+		var field string
+
+		fields = strings.Fields(lines[i])
+		field = fields[2]
+		machine.ax = StrToInt(field[2 : len(field)-1])
+		machine.ay = StrToInt(fields[3][2:])
+
+		fields = strings.Fields(lines[i+1])
+		field = fields[2]
+		machine.bx = StrToInt(field[2 : len(field)-1])
+		machine.by = StrToInt(fields[3][2:])
+
+		fields = strings.Fields(lines[i+2])
+		field = fields[1]
+		machine.prizex = 10_000_000_000_000 + StrToInt(field[2:len(field)-1])
+		machine.prizey = 10_000_000_000_000 + StrToInt(fields[2][2:])
+
+		machines = append(machines, machine)
+	}
+
+	sum := 0
+	for _, machine := range machines {
+		if cost, ok := getCost2(machine); ok {
+			sum += cost
+		}
+	}
+
+	fmt.Println(sum)
 }
